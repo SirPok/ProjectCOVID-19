15a5e8615a26b7fe3fc292a4b2b60a124fdf78ed
SirPok <s6030211031@phuket.psu.ac.th>
2020-04-27 21:40:33 +0700

    ปรับหน้าแก้ไขข้อมูล

--- country.js
+++ country.js
@@ -41,59 +41,120 @@ $(function(){
         $("#Recovered").append(selectedCountry[selectedCountry.length-1].recovered);
 
         let myChart = document.getElementById('myChart').getContext('2d');
+
+        
         
         let massPopChart = new Chart(myChart, {
 
 
             type : 'line',
             data:{
-                labels:['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+                labels:[
+                    
+                    selectedCountry[selectedCountry.length-10].date,
+                    selectedCountry[selectedCountry.length-9].date,
+                    selectedCountry[selectedCountry.length-8].date,
+                    selectedCountry[selectedCountry.length-7].date,
+                    selectedCountry[selectedCountry.length-6].date, 
+                    selectedCountry[selectedCountry.length-5].date, 
+                    selectedCountry[selectedCountry.length-4].date, 
+                    selectedCountry[selectedCountry.length-3].date, 
+                    selectedCountry[selectedCountry.length-2].date, 
+                    selectedCountry[selectedCountry.length-1].date],
+
                 datasets:[{
-                    label : 'Confirm',
+                    label : 'Confirmed',
                     backgroundColor :[
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
+                        '#ecb800',
+                        '#ecb800',
+                        '#ecb800',
+                        '#ecb800',
+                        '#ecb800',
+                        '#ecb800',
                     ],
                     
                     borderColor:[
-                        'rgba(255, 99, 132)'
+                        '#ecb800',
                     ],
                     data:[
-                            617594,
-                            181045,
-                            153060,
-                            106519,
-                            105162,
-                            95072
+                            
+                        selectedCountry[selectedCountry.length-10].confirmed,
+                        selectedCountry[selectedCountry.length-9].confirmed,
+                        selectedCountry[selectedCountry.length-8].confirmed,
+                        selectedCountry[selectedCountry.length-7].confirmed,
+                        selectedCountry[selectedCountry.length-6].confirmed,
+                        selectedCountry[selectedCountry.length-5].confirmed,
+                        selectedCountry[selectedCountry.length-4].confirmed,
+                        selectedCountry[selectedCountry.length-3].confirmed,
+                        selectedCountry[selectedCountry.length-2].confirmed,
+                        selectedCountry[selectedCountry.length-1].confirmed
+
                     ],
                     fill:false,
                 },{
-                    label : 'Thailand',
+                    label : 'Deaths',
                     backgroundColor :[
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
-                        'rgba(255, 99, 132)',
+
+                        '#d22d36',
+                        '#d22d36',
+                        '#d22d36',
+                        '#d22d36',
+                        '#d22d36',
+                        '#d22d36',
                     ],
                     
                     borderColor:[
-                        'rgba(255, 99, 132)'
+                        '#d22d36',
                     ],
                     data:[
-                            95072,
-                            105162,
-                            153060,
-                            153060,
-                            181045,
-                            617594
+
+                        selectedCountry[selectedCountry.length-10].deaths,
+                        selectedCountry[selectedCountry.length-9].deaths,
+                        selectedCountry[selectedCountry.length-8].deaths,
+                        selectedCountry[selectedCountry.length-7].deaths,
+                        selectedCountry[selectedCountry.length-6].deaths,
+                        selectedCountry[selectedCountry.length-5].deaths,
+                        selectedCountry[selectedCountry.length-4].deaths,
+                        selectedCountry[selectedCountry.length-3].deaths,
+                        selectedCountry[selectedCountry.length-2].deaths,
+                        selectedCountry[selectedCountry.length-1].deaths
+
                     ],
                     fill:false,
+                },{
+
+                    label : 'Recovered',
+                    backgroundColor :[
+                        '#039245',
+                        '#039245',
+                        '#039245',
+                        '#039245',
+                        '#039245',
+                        '#039245',
+                    ],
+                    
+                    borderColor:[
+                        '#039245',
+                    ],
+                    data:[
+
+                        selectedCountry[selectedCountry.length-10].recovered,
+                        selectedCountry[selectedCountry.length-9].recovered,
+                        selectedCountry[selectedCountry.length-8].recovered,
+                        selectedCountry[selectedCountry.length-7].recovered,
+                        selectedCountry[selectedCountry.length-6].recovered,
+                        selectedCountry[selectedCountry.length-5].recovered,
+                        selectedCountry[selectedCountry.length-4].recovered,
+                        selectedCountry[selectedCountry.length-3].recovered,
+                        selectedCountry[selectedCountry.length-2].recovered,
+                        selectedCountry[selectedCountry.length-1].recovered
+
+
+                    ],
+
+                    fill:false,
+
+
                 }]
                 
             },
